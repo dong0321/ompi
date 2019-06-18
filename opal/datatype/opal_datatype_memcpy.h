d6aeabb063e937bda7a65a1094dc54219f4e4b6c
Dong <dzhong@vols.utk.edu>
2019-06-18 11:18:47 -0500

    update memcpy using arm sve load/store

--- opal/datatype/opal_datatype_memcpy.h
+++ opal/datatype/opal_datatype_memcpy.h
@@ -14,7 +14,40 @@
 #ifndef OPAL_DATATYPE_MEMCPY_H_HAS_BEEN_INCLUDED
 #define OPAL_DATATYPE_MEMCPY_H_HAS_BEEN_INCLUDED
 
-#define MEMCPY( DST, SRC, BLENGTH ) \
-    memcpy( (DST), (SRC), (BLENGTH) )
+#ifdef __ARM_FEATURE_SVE
+#include <arm_sve.h>
+#endif /* __ARM_FEATURE_SVE */
 
+#include <stdio.h>
+
+#define MEMCPY( DST, SRC, BLENGTH )                                 \
+    do {                                                            \
+        if( (BLENGTH) < 16 ) {                                      \
+            uintptr_t align = ((uintptr_t)(DST)) ^ ((uintptr_t)(SRC));                        \
+            if( (4 == (BLENGTH)) && (0 == (align & 0x3)) ) {  /* We are copying an int */     \
+                *(int*)(DST) = *(int*)(SRC);                        \
+            } else if( (8 == (BLENGTH)) && (0 == (align & 0x7)) ) {   /* We are copying a double */   \
+                *(double*)(DST) = *(double*)(SRC);                  \
+            } else if( (16 == (BLENGTH)) && (0 == (align & 0xF)) ) {   /* We are copying a long double */   \
+                *(long double*)(DST) = *(long double*)(SRC);        \
+            } else {                                                \
+                memcpy((DST), (SRC), (BLENGTH));                    \
+            }                                                       \
+        } else {                                                    \
+            /* min value when length > 4* VL */                     \
+            if( (BLENGTH) >= svcntb() * 4 ) {                       \
+                svbool_t Pg = svptrue_b8();                         \
+                uint64_t i;                                         \
+                for(i=0; i<BLENGTH; i+=svcntb() * 4)                \
+                {   /*printf("H%d,%d ", BLENGTH, i); */             \
+                    svuint8x4_t  vsrc = svld4(Pg, (uint8_t*)(SRC + i) );           \
+                    svst4(Pg, (uint8_t*)(DST + i), vsrc);               \
+                }                                                       \
+                Pg = svwhilelt_b8_u64(i, BLENGTH);                 \
+                svuint8x4_t  vsrc = svld4(Pg, (uint8_t*)(SRC+i));  \
+                svst4(Pg, (uint8_t*)(DST+i), vsrc);                \
+            }                                                           \
+            else { memcpy((DST), (SRC), (BLENGTH)); }                   \
+        }                                                               \
+    } while (0)
 #endif  /* OPAL_DATATYPE_MEMCPY_H_HAS_BEEN_INCLUDED */
