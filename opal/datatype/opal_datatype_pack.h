80ee6e503f7aca02c6715f8c12f10ffeb7bbaf7f
Dong Zhong <zhongdong0321@hotmail.com>
2021-02-15 13:02:34 -0500

    add avx gather and scatter

--- opal/datatype/opal_datatype_pack.h
+++ opal/datatype/opal_datatype_pack.h
@@ -20,12 +20,21 @@
 
 #include "opal_config.h"
 
+#include "opal_config.h"
+#include <immintrin.h>
+
 #if !defined(CHECKSUM) && OPAL_CUDA_SUPPORT
 /* Make use of existing macro to do CUDA style memcpy */
 #undef MEMCPY_CSUM
 #define MEMCPY_CSUM( DST, SRC, BLENGTH, CONVERTOR ) \
     CONVERTOR->cbmemcpy( (DST), (SRC), (BLENGTH), (CONVERTOR) )
 #endif
+
+
+#define opal_datatype_pack_threshold 16
+
+#define vector_bits 512
+#define vector_bytes 512/8
 
 /**
  * This function deals only with partial elements. The COUNT points however to the whole leftover count,
@@ -108,7 +117,7 @@ pack_predefined_data( opal_convertor_t* CONVERTOR,
     *(COUNT) -= cando_count;
 
     /* buffers aligned and we manipulate, on each copy, less data than a defined threshold */
-    if( (0 == ((_memory ^ _packed) & (blocklen_bytes - 1))) && (_elem->blocklen <= opal_datatype_pack_threshold) ) {
+    if( (0 == (( (uintptr_t)_memory ^ (uintptr_t)_packed) & (blocklen_bytes - 1))) && (_elem->blocklen <= opal_datatype_pack_threshold) ) {
         if( 1 == _elem->blocklen ) { /* Do as many full blocklen as possible */
             for(; cando_count > 0; cando_count--) {
                 OPAL_DATATYPE_SAFEGUARD_POINTER( _memory, blocklen_bytes, (CONVERTOR)->pBaseBuf,
@@ -125,18 +134,74 @@ pack_predefined_data( opal_convertor_t* CONVERTOR,
         if( (1 < _elem->count) && (_elem->blocklen <= cando_count) ) {
             blocklen_bytes *= _elem->blocklen;
 
-            do { /* Do as many full blocklen as possible */
-                OPAL_DATATYPE_SAFEGUARD_POINTER( _memory, blocklen_bytes, (CONVERTOR)->pBaseBuf,
-                                                 (CONVERTOR)->pDesc, (CONVERTOR)->count );
-                DO_DEBUG( opal_output( 0, "pack 2. memcpy( %p, %p, %lu ) => space %lu\n",
-                                       (void*)_packed, (void*)_memory, (unsigned long)blocklen_bytes, (unsigned long)(*(SPACE) - (_packed - *(packed))) ); );
-                MEMCPY_CSUM( _packed, _memory, blocklen_bytes, (CONVERTOR) );
-                _packed     += blocklen_bytes;
-                _memory     += _elem->extent;
-                cando_count -= _elem->blocklen;
-            } while (_elem->blocklen <= cando_count);
-        }
+            if (vector_bytes < blocklen_bytes*2) {
+                do { /* Do as many full blocklen as possible */
+                    OPAL_DATATYPE_SAFEGUARD_POINTER( _memory, blocklen_bytes, (CONVERTOR)->pBaseBuf,
+                            (CONVERTOR)->pDesc, (CONVERTOR)->count );
+                    DO_DEBUG( opal_output( 0, "pack 2. memcpy( %p, %p, %lu ) => space %lu\n",
+                                (void*)_packed, (void*)_memory, (unsigned long)blocklen_bytes, (unsigned long)(*(SPACE) - (_packed - *(packed))) ); );
+                    MEMCPY_CSUM( _packed, _memory, blocklen_bytes, (CONVERTOR) );
+                    _packed     += blocklen_bytes;
+                    _memory     += _elem->extent;
+                    cando_count -= _elem->blocklen;
+                } while (_elem->blocklen <= cando_count);
+            }
+            else {
+                // avx gather pack
+                int blocks_in_vl = vector_bytes/blocklen_bytes;
 
+                if (blocks_in_vl > cando_count){
+                    blocks_in_vl =  cando_count;
+                }
+                /* max VL 512/8/4 = 16 offsets for MPI_INT  */
+                uint32_t off_sets[16];
+                int start = 0;
+                for(int j=0; j<blocks_in_vl; j++){
+                    for(size_t i =0; i<blocklen_bytes/opal_datatype_basicDatatypes[_elem->common.type]->size; i++)
+                    {
+                        /* offset in bytes offset=offset in block + extend(bytes)*j */
+                        off_sets[start] = (i+j*_elem->extent/opal_datatype_basicDatatypes[_elem->common.type]->size);
+                        DO_DEBUG( opal_output( 0, "off_sets --%d",off_sets[start]););
+                        start++;
+                    }
+                }
+                /* cannot totally fullfill the vector but almost full, best we can do */
+                __mmask16 load_mask;
+                __m512i temp_src;
+                __m512i xt = _mm512_load_epi32((__m512*)off_sets);
+                int num_of_copys = cando_count/ (_elem->blocklen*blocks_in_vl);
+                load_mask = _cvtu32_mask16((1<<_elem->blocklen*blocks_in_vl)-1);
+                for(int i=0; i < num_of_copys; i++)
+                {
+                    DO_DEBUG( opal_output( 0, "pack full VL. memcpy( %p, %p, %lu ) => space %lu copy seq %d (cando %d)(len %d)(extend %d)"
+                                ,(void*)*(packed), (void*)_memory, (unsigned long)blocklen_bytes*blocks_in_vl, (unsigned long)*(SPACE),
+                                i, cando_count, _elem->blocklen, _elem->extent); );
+                    __m512i vsrc = _mm512_mask_i32gather_epi32 (temp_src, load_mask, xt, (void*)_memory, 4);
+                    _mm512_store_epi32 (*(packed), vsrc);
+                    _packed     += blocklen_bytes*blocks_in_vl;
+                    _memory     += _elem->extent*blocks_in_vl;
+                    cando_count -= _elem->blocklen*blocks_in_vl;
+                }
+                /* remaining blocks */
+                blocks_in_vl = cando_count / _elem->blocklen;
+                DO_DEBUG( opal_output( 0, "Total ramining cando_count -- %d", cando_count););
+                if (blocks_in_vl != 0) {
+                    // Need to use mask OP for partial load & store
+                    __mmask16 load_mask = _cvtu32_mask16((1<<cando_count)-1);
+                    __m512i vsrc = _mm512_mask_i32gather_epi32 (temp_src, load_mask, xt,  (void*)_memory, 4);
+                    _mm512_mask_store_epi32( *(packed), load_mask, vsrc);
+
+                    for(int i=0; i<_elem->blocklen*blocks_in_vl; i++)
+                    {
+                    //    DO_DEBUG( opal_output( 0, "ramining No.%d -- %p %d  %p %d ", i, _packed+i*4,*(_packed+i*4),_memory+i ,*(_memory+i)););
+                    }
+
+                    _packed     += blocklen_bytes*blocks_in_vl;
+                    _memory     += _elem->extent*blocks_in_vl;
+                    cando_count -= _elem->blocklen*blocks_in_vl;
+                }
+            }
+        }
         /**
          * As an epilog do anything left from the last blocklen.
          */
@@ -152,6 +217,7 @@ pack_predefined_data( opal_convertor_t* CONVERTOR,
             _memory   += do_now_bytes;
             _packed   += do_now_bytes;
         }
+        goto update_and_return;
     }
     if( 1 == _elem->blocklen ) { /* Do as many full blocklen as possible */
         for(; cando_count > 0; cando_count--) {
